27d2ef78c6d08e3070dcbf557abdd73b1d92acef
UeliHaltner <ul528383@dal.ca>
2019-02-22 19:10:29 -0400

    ListView and addButton added
    - added List View to home UI
    - list view still reqires components
    
    - added addButton to home UI
    - button padding needs adjusting on bottom of screen
    
    -added onclick listener for addButton
    -viewlist requires onitemclicklistener
    -pseudo code added for gps

--- app/src/main/java/walkwithme/mc/dal/com/walkwithme/HomeActivity.java
+++ app/src/main/java/walkwithme/mc/dal/com/walkwithme/HomeActivity.java
@@ -1,13 +1,59 @@
 package walkwithme.mc.dal.com.walkwithme;
 
+import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Activity mActivty;
+
+    ListView walkList;
+    FloatingActionButton addFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mActivty = this;
+        walkList = findViewById(R.id.list_walks);
+        addFab = findViewById(R.id.addFab);
+
+        //get current GPS location
+
+        //if location is given
+
+            //get list of walks
+
+            //for each walk get coordinate and compare to current location
+
+                //sort list by nearest walks
+
+
+        //else if location is not giver
+
+            //random order or by date
+
+
+
+        //Create a ListOnClickListener for walkList
+
+
+        //Create click listener for add button
+        addFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //change event
+
+            }
+        });
+
+
     }
+
 }
