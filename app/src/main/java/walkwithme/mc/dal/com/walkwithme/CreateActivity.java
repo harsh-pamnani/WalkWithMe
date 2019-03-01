c3d13e9e4dcd158691abc75fbe9f3e7508d87acd
Deep Shah <deep.shah_4@yahoo.com>
2019-03-01 18:32:43 -0400

    Date picker, Time Picker implemented

--- app/src/main/java/walkwithme/mc/dal/com/walkwithme/CreateActivity.java
+++ app/src/main/java/walkwithme/mc/dal/com/walkwithme/CreateActivity.java
@@ -1,9 +1,20 @@
 package walkwithme.mc.dal.com.walkwithme;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -11,16 +22,48 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import android.widget.ImageView;
+import android.widget.TimePicker;
+
 public class CreateActivity extends AppCompatActivity {
+
+    ImageView ivImage;
+    Integer REQUEST_CAMERA=1, SELECT_FILE=0;
+
+    AutoCompleteTextView text;
+    String[] places ={"Dalhousie ","SMU","Duncan Street","Park Victoria","Robbie Street","Cunnard Street"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+        text=(AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+
+        ArrayAdapter adapter = new
+                ArrayAdapter(this,android.R.layout.simple_list_item_1, places);
+
+        text.setAdapter(adapter);
+        text.setThreshold(1);
+
         final Calendar myCalendar = Calendar.getInstance();
 
-        final EditText edittext= (EditText) findViewById(R.id.lblDateEdit);
+        final EditText edittext = (EditText) findViewById(R.id.lblDateEdit);
+        final EditText edittime = (EditText) findViewById(R.id.lblTimeEdit);
+
+        ivImage = (ImageView) findViewById(R.id.lblUpload);
+
+        Button fab = (Button) findViewById(R.id.lblUploadButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SelectImage();
+            }
+        });
+
+
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
             @Override
@@ -45,12 +88,92 @@ public class CreateActivity extends AppCompatActivity {
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        edittime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(CreateActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        edittime.setText(hourOfDay + ":" + minutes);
+                    }
+                }, 0, 0, false);
+
+                timePickerDialog.show();
+
+            }
+        });
+
+
     }
 
+
+    private void SelectImage(){
+
+        final CharSequence[] items={"Camera","Gallery", "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(CreateActivity.this);
+        builder.setTitle("Add Image");
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (items[i].equals("Camera")) {
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+
+                } else if (items[i].equals("Gallery")) {
+
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, SELECT_FILE);
+
+                } else if (items[i].equals("Cancel")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+
+
+
     private void updateLabel(EditText edittext, Calendar myCalendar) {
-        String myFormat = "dd/MMM/yyyy"; //In which you need put here
+        String myFormat = "dd/mm/yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
         edittext.setText(sdf.format(myCalendar.getTime()));
+
     }
+
+
+    @Override
+    public  void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode,data);
+
+        if(resultCode== Activity.RESULT_OK){
+
+            if(requestCode==REQUEST_CAMERA){
+
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ivImage.setImageBitmap(bmp);
+
+            }else if(requestCode==SELECT_FILE){
+
+                Uri selectedImageUri = data.getData();
+                ivImage.setImageURI(selectedImageUri);
+            }
+
+        }
+    }
+
+
+
+
+
 }
