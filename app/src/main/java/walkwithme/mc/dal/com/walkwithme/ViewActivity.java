b10a45d539c5893ca4e85b59eaaeb8e1ed2d01e0
nbhardwaj <nitish_bhardwaj@dal.ca>
2019-03-26 16:47:43 -0300

    Code backup for Images

--- app/src/main/java/walkwithme/mc/dal/com/walkwithme/ViewActivity.java
+++ app/src/main/java/walkwithme/mc/dal/com/walkwithme/ViewActivity.java
@@ -1,8 +1,8 @@
 package walkwithme.mc.dal.com.walkwithme;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -16,11 +16,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import walkwithme.mc.dal.com.walkwithme.ActivityJsonObj.ViewActivityJsonObj;
 
 public class ViewActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -41,6 +36,13 @@ public class ViewActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     //Fetching Images from the Drawable directory
     int[] sampleImages = {R.drawable.walk_2, R.drawable.walk_1, R.drawable.walk_3, R.drawable.walk_4};
+    private String[] imageUrls = new String[]{
+            "https://cdn.pixabay.com/photo/2016/11/11/23/34/cat-1817970_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/12/21/12/26/glowworm-3031704_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/12/24/09/09/road-3036620_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/10/10/15/28/butterfly-2837589_960_720.jpg"
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -83,9 +85,12 @@ public class ViewActivity extends AppCompatActivity implements OnMapReadyCallbac
         eventNameView.setText(eventName);
 
         //Creating the carousel View for the event images
-        carouselView = (CarouselView) findViewById(R.id.carousel_view);
-        carouselView.setPageCount(sampleImages.length);
-        carouselView.setImageListener(imageListener);
+//        carouselView = (CarouselView) findViewById(R.id.carousel_view);
+//        carouselView.setPageCount(sampleImages.length);
+//        carouselView.setImageListener(imageListener);
+        ViewPager viewPager = findViewById(R.id.view_pager_Image);
+        PagerViewAdapter adapter = new PagerViewAdapter(this, imageUrls);
+        viewPager.setAdapter(adapter);
 
         // Setting the location coordinates in google Map
         eventLoc = new LatLng(eventCoordinateLang, eventCoordinateLong);
