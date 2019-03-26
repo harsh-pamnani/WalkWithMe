573f34a3271b749123cc7ccc978cc3e143362d41
hpamnani <harshpam1993@gmail.com>
2019-03-25 23:46:12 -0300

    adding instance variable for id, images URL, locaiton, date and time

--- app/src/main/java/walkwithme/mc/dal/com/walkwithme/CreateActivityForm.java
+++ app/src/main/java/walkwithme/mc/dal/com/walkwithme/CreateActivityForm.java
@@ -1,6 +1,46 @@
 package walkwithme.mc.dal.com.walkwithme;
 
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateActivityForm {
+
+    String id;
+    String title;
+    String location;
+    String date;
+    String time;
+    String description;
+    ArrayList<String> imageURL;
+
+    public ArrayList<String> getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(ArrayList<String> imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
     public String getTitle() {
         return title;
     }
@@ -9,11 +49,38 @@ public class CreateActivityForm {
         this.title = title;
     }
 
-    String title;
+    public String getLocation() {
+        return location;
+    }
 
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 
     // Constructor for Create Activity form
-    public CreateActivityForm(String title) {
+    public CreateActivityForm(String id, String title, String location, String date, String time, String description, ArrayList<String> imageURL) {
+        this.id = id;
         this.title = title;
+        this.location = location;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.imageURL = imageURL;
     }
 }
